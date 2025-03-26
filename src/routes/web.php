2b72eec7b068b5e1a8d8ebaac409b135f393cea2
kaz1877 <k689208@gmail.com>
2025-03-27 01:40:01 +0900

    エラー個所の修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,12 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/recipes', [RecipeController::class, 'index']);
-Route::post('/recipes', [RecipeController::class, 'store']);
-Route::get('/recipes/{id}', [RecipeController::class, 'show']);
-Route::put('/recipes/{id}', [RecipeController::class, 'update']);
-Route::delete('/recipe/{id}', [RecipeController::class, 'delete']);
+Route::get('/GET recipes', [RecipeController::class, 'index']);
+Route::post('/POST recipes', [RecipeController::class, 'store']);
+Route::get('/GET/{id} recipe', [RecipeController::class, 'show']);
+Route::put('/PATCH/{id} recipe', [RecipeController::class, 'update']);
+Route::delete('/DELETE/{id} recipe', [RecipeController::class, 'delete']);
