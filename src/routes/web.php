a0d3f430086c0b03a7d98a82a740f367bb75704d
kaz1877 <k689208@gmail.com>
2025-03-27 02:03:25 +0900

    修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,5 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/recipes', [RecipeController::class, 'index']);
 Route::post('/recipes', [RecipeController::class, 'store']);
 Route::get('/recipes/{id}', [RecipeController::class, 'show']);
-Route::put('/recipes/{id}', [RecipeController::class, 'update']);
+Route::patch('/recipes/{id}', [RecipeController::class, 'update']);
 Route::delete('/recipes/{id} ', [RecipeController::class, 'delete']);
